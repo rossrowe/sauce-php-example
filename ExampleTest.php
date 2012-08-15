3a421219d03b4bc9ada0312f6d82448665ded117
Ross Rowe <piaross@gmail.com>
2012-08-15 21:17:15 +1000

    Reference remaining environment variables

--- ExampleTest.php
+++ ExampleTest.php
@@ -8,9 +8,9 @@ class ExampleTest extends PHPUnit_Extensions_SeleniumTestCase_SauceOnDemandTestC
     function setUp() {
         $this->setUsername(getenv('SAUCE_USER_NAME'));
         $this->setAccessKey(getenv('SAUCE_API_KEY'));
-        $this->setOs('Windows 2003');
-        $this->setBrowser('firefox');
-        $this->setBrowserVersion('3.6.');
+        $this->setOs(getenv('SELENIUM_PLATFORM'));
+        $this->setBrowser(getenv('SELENIUM_BROWSER'));
+        $this->setBrowserVersion(getenv('SELENIUM_VERSION'));
         $this->setBrowserUrl('http://example.saucelabs.com');
     }
 
